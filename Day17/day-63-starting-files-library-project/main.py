65e053a70a109afa3f55a10630ab64291f0b9e98
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2025-12-13 23:21:36 -0700

    updated day 17

---
+++ Day17/day-63-starting-files-library-project/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    
+
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    
+    if request.method == "POST":
+    
+        data={'title':request.form["book name"],
+              "author":request.form["book author"],
+              "rating":request.form["rating"]
+              }
+        
+        all_books.append(data)
+        return redirect(url_for('home'))
+        
+
+
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
