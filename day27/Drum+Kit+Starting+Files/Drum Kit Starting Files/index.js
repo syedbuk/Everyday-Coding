f7b93d90a19fdd91eac6b57a410bb376bcc14a82
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-01-04 09:18:24 -0700

    Add initial styles and assets for Drum Kit project
    - Created styles.css with basic styling for the drum kit interface, including body, headings, and button styles.
    - Added various image assets for drum sounds (kick, snare, toms, crash) in PNG format.
    - Included sound files (MP3) for each drum sound effect.
    - Added MacOS metadata files for compatibility.

---
+++ day27/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,36 @@
+
+function playsound(key){
+
+    switch (key) {
+       
+        
+
+        case:'w':
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case:'a':
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case:'s':
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case:'d':
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;          
+
+
+
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click',function(event){
+
+        var buttonInnerHTML = this.innerHTML;
+
+        playsound(buttonInnerHTML);
+
+    })
