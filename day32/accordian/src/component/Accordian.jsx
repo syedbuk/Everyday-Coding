1ff4b8be44d52444eb263ab7197223cfabed3a94
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-05-14 20:55:13 -0600

    feat: implement multi-selection functionality in Accordian component

--- day32/accordian/src/component/Accordian.jsx
+++ day32/accordian/src/component/Accordian.jsx
@@ -8,6 +8,8 @@ import "./styles.css";
 export default function Accordian() {
 
     const [selection, setSelection] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multipleSelection, setMultipleSelection] = useState([]);
 
 
     function handleSingleSelection(id){
@@ -17,28 +19,47 @@ export default function Accordian() {
 
     }
 
+    function handleMultipleSelection(id){
+        let cpyMultiple= {...multipleSelection};
+        const findIndexofCurrentId = cpyMultiple.indexOf(getCurrentId)
+
+        console.log(findIndexofCurrentId);
+
+        if (findIndexofCurrentId === -1) {
+            cpyMultiple.push(id);
+        } else {
+            cpyMultiple.splice(findIndexofCurrentId, 1);
+
+
+        }
+
+        setMultipleSelection(cpyMultiple)
+
+    }
+
 
 
 
     return <div className="wrapper">
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
         <div className="accordian">
 
             {data && data.length > 0 ? data.map(dataItem =>
 
                 <div className="item">
 
-                    <div onClick ={handleSingleSelection(dataItem.id)}className="title">
+                    <div onClick ={enableMultiSelection ? () => handleMultipleSelection(dataItem.id) : () => handleSingleSelection(dataItem.id)}className="title">
                         <h3>{dataItem.question}</h3>
 
                     </div><span>+</span>
 
                    {
-                    selection === dataItem.id?<div>{dataItem.answer}</div>:null
+                    enableMultiSelection?multipleSelection.includes(dataItem.id)?<div>{dataItem.answer}</div>:null:selection === dataItem.id?<div>{dataItem.answer}</div>:null
                    }
 
-                </div>,) : <div>No data found! </div>
+                </div>) : <div>No data found! </div>
                     
-                
+}
                 
                 
                 </div>
